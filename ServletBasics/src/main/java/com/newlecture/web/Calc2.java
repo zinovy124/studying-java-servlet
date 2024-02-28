db73d62c0885a9009ab1add1be0c62652939e6d7
zinovy124 <developerzinovy@gmail.com>
2024-02-29 08:22:53 +0900

    2024-02-27

--- ServletBasics/src/main/java/com/newlecture/web/Calc2.java
+++ ServletBasics/src/main/java/com/newlecture/web/Calc2.java
@@ -6,15 +6,19 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ServletContext application = req.getServletContext();
+		HttpSession session = req.getSession();
+		Cookie[] cookies = req.getCookies();
 		
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; charset=UTF-8");
@@ -27,9 +31,28 @@ public class Calc2 extends HttpServlet {
 		
 		// 값 계산
 		if (op.equals("=")) {
-			int x = (Integer)application.getAttribute("value");
+//			int x = (Integer)application.getAttribute("value");
+//			int x = (Integer)session.getAttribute("value");
+			int x = 0;
+			for (Cookie c : cookies) {
+				if (c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+					break;
+				}
+			}
+			
 			int y = v;
-			String operator = (String)application.getAttribute("op");
+//			String operator = (String)application.getAttribute("op");
+//			String operator = (String)session.getAttribute("op");
+			String operator = "";
+			
+			for (Cookie c : cookies) {
+				if (c.getName().equals("op")) {
+					operator = c.getValue();
+					break;
+				}
+			}
+			
 			int result = 0;
 			
 			if (operator.equals("+")) {
@@ -41,8 +64,16 @@ public class Calc2 extends HttpServlet {
 			resp.getWriter().printf("result is %d\n", result);
 		// 값 저장
 		} else {
-			application.setAttribute("value", v);
-			application.setAttribute("op", op);
+//			application.setAttribute("value", v);
+//			application.setAttribute("op", op);
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			resp.addCookie(valueCookie);
+			resp.addCookie(opCookie);
+			
 		}
 		
 	}
